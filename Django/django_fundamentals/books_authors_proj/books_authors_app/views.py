e4b67dca3e8f2693fde42a0793fe5d62b1b1f5b2
Mohamamd Yahya <mohammad1.yahya2019@gmail.com>
2023-12-03 16:37:03 +0200

    Add a Book

--- Django/django_fundamentals/books_authors_proj/books_authors_app/views.py
+++ Django/django_fundamentals/books_authors_proj/books_authors_app/views.py
@@ -1,26 +1,54 @@
-from django.shortcuts import render
-from flask import redirect
-from.models import Book,Author
+from django.shortcuts import redirect, render, HttpResponse
+from .models import Book, Author
+
 def index(request):
-    context ={
-
-"Book" : Book.objects.all(),
-    }
-    return render(request,"index.html",context)
-def show(request):
-    title =request.POST['title'] 
-    desc =request.POST['desc']
-    Book.objects.create(title=title, desc=desc)
-    return redirect("/")
-
-
-def view_book(request,id):
-    Book = Book.objects.get(id=id)
-    Authors = Book.Authors.all()
-    Books_authors = Author.objects.get(books=Book)
     context = {
-        "Books": Book,
-        "authors": Authors,
-        " Books_authors":  Books_authors
+        "all_the_books": Book.objects.all()
     }
-    return render(request, "show.html", context)
+    return render(request, "index.html", context)
+
+def new_book(request):
+    Book.objects.create(title = request.POST['title'], desc = request.POST['desc'])
+    return redirect('/')
+
+
+def view_book(request, book_id):            
+    context = {
+        "this_book": Book.objects.get(id = book_id),
+        "all_the_authors": Author.objects.filter(books = book_id)
+    }
+    return render(request, "view_book.html", context)
+
+def add_author(request):
+    author_to_add = Author.objects.get(id = request.POST['author_id'])
+    book = Book.objects.get(id = request.POST['book_id'])
+    author_to_add.books.add(book)
+    book_id = request.POST['book_id']
+    return redirect(f'/books/{book_id}')
+
+def authors(request):
+    context = {
+        "all_the_authors": Author.objects.all()
+    }
+    return render(request, "authors.html", context)
+
+def new_author(request):
+    Author.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], notes = request.POST['notes'])
+    return redirect('/authors')
+
+
+def view_author(request, author_id):       
+    context = {
+        "this_author": Author.objects.get(id = author_id),
+        "all_the_books": Book.objects.filter(authors = author_id)
+    }
+    return render(request, "view_author.html", context)
+
+
+
+def add_book(request):
+    book_to_add = Book.objects.get(id = request.POST['book_id'])
+    author = Author.objects.get(id = request.POST['author_id'])
+    book_to_add.authors.add(author)
+    author_id = request.POST['author_id']
+    return redirect(f'/authors/{author_id}')
